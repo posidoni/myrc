1bec51cba235f2296243cff9383ad3f5ce0935d8
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-28 19:44:42 +0300

    [Sync] - Tue Jun 28 19:44:42 2022

--- configs/nvim/lua/user/treesitter.lua
+++ configs/nvim/lua/user/treesitter.lua
@@ -11,7 +11,7 @@ configs.setup {
     enable = true,
   },
   highlight = {
-    enable = true, -- false will disable the whole extension
+    enable = disable, -- false will disable the whole extension
     disable = { "markdown" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
