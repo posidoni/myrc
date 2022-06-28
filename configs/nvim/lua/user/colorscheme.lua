9eb9dee8e844c7b1e49fff821423f6d2ea34d712
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-28 19:54:32 +0300

    [Sync] - Tue Jun 28 19:54:31 2022

--- configs/nvim/lua/user/colorscheme.lua
+++ configs/nvim/lua/user/colorscheme.lua
@@ -1,71 +1,29 @@
--- require('onedark').setup  {
---     -- Main options --
---     style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
---     transparent = false,  -- Show/hide background
---     term_colors = true, -- Change terminal color as per the selected theme style
---     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
---     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+require('onedark').setup  {
+    -- Main options --
+    style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,  -- Show/hide background
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
---     -- Change code style ---
---     -- Options are italic, bold, underline, none
---     -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
---     code_style = {
---         comments = 'italic',
---         keywords = 'none',
---         functions = 'bold',
---         strings = 'italic',
---         variables = 'none'
---     },
+    -- Change code style ---
+    -- Options are italic, bold, underline, none
+    -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+    code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'bold',
+        strings = 'italic',
+        variables = 'none'
+    },
 
---     colors = {
---         bright_orange = "#ff8800",    -- define a new color
--- 	},
--- 	highlights = {
--- 		TSKeyword = {fg = '$green'},
--- 		TSString = {fg = '$bright_orange', bg = '#00ff00', fmt = 'bold'},
--- 		TSFunction = {fg = '#0000ff', sp = '$cyan', fmt = 'underline,italic'},
--- 	},
---     -- Plugins Config --
---     diagnostics = {
---         darker = true, -- darker colors for diagnostic
---         undercurl = true,   -- use undercurl instead of underline for diagnostics
---         background = true,    -- use background color for virtual text
---     },
--- }
+    -- Plugins Config --
+    diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true,   -- use undercurl instead of underline for diagnostics
+        background = true,    -- use background color for virtual text
+    },
+}
 
--- require('onedark').load()
+require('onedark').load()
 
-local onedarkpro = require("onedarkpro")
-onedarkpro.setup({
-  dark_theme = "onedark", -- The default dark theme
-  light_theme = "onelight", -- The default light theme
-  -- Theme can be overwritten with 'onedark' or 'onelight' as a string
-  colors = {}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
-  hlgroups = {}, -- Override default highlight groups
-  filetype_hlgroups = {}, -- Override default highlight groups for specific filetypes
-  plugins = { -- Override which plugins highlight groups are loaded
-      native_lsp = true,
-      polygot = true,
-      treesitter = true,
-      -- NOTE: Other plugins have been omitted for brevity
-  },
-  styles = {
-      strings = "NONE", -- Style that is applied to strings
-      comments = "NONE", -- Style that is applied to comments
-      keywords = "NONE", -- Style that is applied to keywords
-      functions = "NONE", -- Style that is applied to functions
-      variables = "NONE", -- Style that is applied to variables
-      virtual_text = "NONE", -- Style that is applied to virtual text
-  },
-  options = {
-      bold = false, -- Use the themes opinionated bold styles?
-      italic = false, -- Use the themes opinionated italic styles?
-      underline = false, -- Use the themes opinionated underline styles?
-      undercurl = false, -- Use the themes opinionated undercurl styles?
-      cursorline = false, -- Use cursorline highlighting?
-      transparency = false, -- Use a transparent background?
-      terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
-      window_unfocussed_color = false, -- When the window is out of focus, change the normal background?
-  }
-})
-onedarkpro.load()
