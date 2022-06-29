41bb5000066f8a336bd8f99f9ff5b19c70b19b61
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-29 03:30:57 +0300

    [Sync] - Wed Jun 29 03:30:57 2022

--- configs/nvim/lua/user/nvim-tree.lua
+++ configs/nvim/lua/user/nvim-tree.lua
@@ -1,166 +1,146 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+    return
 end
 
 nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = true,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = true,
+    ignore_ft_on_setup = {
+        "startify",
+        "dashboard",
+        "alpha",
     },
-  },
-  update_focused_file = {
-    enable = true,
+    open_on_tab = false,
+    hijack_cursor = false,
     update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = "open",
-    args = {},
-  },
-  filters = {
-    dotfiles = true,
-    custom = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 25,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        -- { key = "h", cb = tree_cb "close_node" },
-        -- { key = "v", cb = tree_cb "vsplit" },
-      },
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
     },
-    number = false,
-    relativenumber = false,
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
     },
-    expand_all = {
-      max_folder_discovery = 300,
+    system_open = {
+        cmd = "open",
+        args = {},
     },
-    open_file = {
-      quit_on_open = false, -- @Mikhail: 'true' behaviour feels annoying
-      window_picker = {
-            enable = false,
-      },
+    filters = {
+        dotfiles = true,
+        custom = {},
     },
-    remove_file = {
-      close_window = true,
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
     },
-  },
-  renderer = {
-    add_trailing = false,
-    group_empty = false,
-    highlight_git = true,
-    full_name = false,
-    highlight_opened_files = "all",
-    root_folder_modifier = ":~",
+    view = {
+        width = 25,
+        height = 30,
+        hide_root_folder = false,
+        side = "left",
+        mappings = {
+            custom_only = false,
+            list = {
+                -- { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+                -- { key = "h", cb = tree_cb "close_node" },
+                -- { key = "v", cb = tree_cb "vsplit" },
+            },
+        },
+        number = false,
+        relativenumber = false,
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true,
+    },
+    actions = {
+        use_system_clipboard = true,
+        change_dir = {
+            enable = true,
+            global = false,
+            restrict_above_cwd = false,
+        },
+        expand_all = {
+            max_folder_discovery = 300,
+        },
+        open_file = {
+            quit_on_open = false, -- @Mikhail: 'true' behaviour feels annoying
+            window_picker = {
+                enable = false,
+            },
+        },
+        remove_file = {
+            close_window = true,
+        },
+    },
+    renderer = {
+        add_trailing = false,
+        group_empty = false,
+        highlight_git = true,
+        full_name = false,
+        highlight_opened_files = "all",
+        root_folder_modifier = ":~",
 
-    -- @Mikhail: this setting draws lines
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        item = "│ ",
-        none = "  ",
-      },
-    },
-    icons = {
-      webdev_colors = true,
-      git_placement = "before",
-      padding = " ",
-      symlink_arrow = " ➛ ",
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
+        -- @Mikhail: this setting draws lines
+        indent_markers = {
+            enable = true,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                item = "│ ",
+                none = "  ",
+            },
         },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
+        icons = {
+            webdev_colors = true,
+            git_placement = "before",
+            padding = " ",
+            symlink_arrow = " ➛ ",
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "",
+                    staged = "",
+                    unmerged = "",
+                    renamed = "➜",
+                    deleted = "",
+                    untracked = "U",
+                    ignored = "◌",
+                },
+            },
         },
-      },
-    },
-    -- @Mikhail: special files are underlined
-    -- This feature looks absolutely great!
-    special_files = {
+        -- @Mikhail: special files are underlined
+        -- This feature looks absolutely great!
+        special_files = {
             "Cargo.toml",
             "Makefile",
             "Dockerfile",
@@ -169,5 +149,5 @@ nvim_tree.setup {
             "CMakeLists.txt",
             "CMakeLists",
         },
-  },
+    },
 }
