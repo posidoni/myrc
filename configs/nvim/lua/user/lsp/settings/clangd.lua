3ae02f27f0f69d419cdbc76a59f72c63d43bafb5
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-29 04:29:25 +0300

    [Sync] - Wed Jun 29 04:29:25 2022

--- configs/nvim/lua/user/lsp/settings/clangd.lua
+++ configs/nvim/lua/user/lsp/settings/clangd.lua
@@ -1,12 +1,27 @@
 return {
     settings = {
-        cmd = { "clangd", "--background-index" },
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--pch-storage=memory",
+            "--clang-tidy",
+            "--suggest-missing-includes",
+            "--cross-file-rename",
+            "--completion-style=detailed",
+        },
+        init_options = {
+            clangdFileStatus = true,
+            usePlaceholders = true,
+            completeUnimported = true,
+            semanticHighlighting = true,
+        },
         filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
         root_dir = {
             '.clangd',
             '.clang-tidy',
             '.clang-format',
             'compile_commands.json',
+            'src/compile_commands.json',
             'compile_flags.txt',
             'configure.ac',
             '.git'
