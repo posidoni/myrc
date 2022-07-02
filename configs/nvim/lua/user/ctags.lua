7136aff17f3efa25753ff9e5f6b2f918b4196205
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-07-02 16:06:20 +0300

    [Sync] - Sat 02 Jul 2022 04:06:20 PM MSK

---
+++ configs/nvim/lua/user/ctags.lua
@@ -0,0 +1,17 @@
+vim.g.gutentags_enabled = 1
+vim.g.gutentags_add_default_project_roots = 1
+vim.g.gutentags_project_root = {
+    'Makefile',
+    'CMakeLists.txt',
+}
+
+vim.g.gutentags_cache_dir = os.getenv('HOME') .. '.cache/vim/ctags'
+vim.g.gutentags_generate_on_new = 1
+vim.g.gutentags_generate_on_missing = 1
+vim.g.gutentags_generate_on_write = 1
+vim.g.gutentags_generate_on_empty_buffer = 0
+
+vim.g.gutentags_ctags_extra_args = {
+    '--tag-relative=yes',
+    '--fields=+ailmnS'
+}
--- configs/nvim/lua/user/plugins.lua
+++ configs/nvim/lua/user/plugins.lua
@@ -65,32 +65,6 @@ packer.init {
     },
 }
 
-
-vim.g.vim_git_sync_branch = 'main'
-
-vim.g.gutentags_add_default_project_roots = 1
-vim.g.gutentags_project_root = {
-    'package.json',
-    'Makefile',
-    '.git',
-    'CMakeLists.txt',
-    'README.md'
-}
-
-vim.g.gutentags_cache_dir = "$HOME/.cache/vim/ctags/"
-vim.g.gutentags_generate_on_new = 1
-vim.g.gutentags_generate_on_missing = 1
-vim.g.gutentags_generate_on_write = 1
-vim.g.gutentags_generate_on_empty_buffer = 0
-
-vim.g.gutentags_ctags_extra_args = {
-    '--tag-relative=yes',
-    '--fields=+ailmnS'
-}
-
-vim.g.tagbar_ctags_bin = '/Volumes/MISHA/mybrew/bin/ctags'
-vim.g.gutentags_ctags_executable = '/Volumes/MISHA/mybrew/bin/ctags'
-
 return packer.startup(function(use)
 
     -- @Performance --
