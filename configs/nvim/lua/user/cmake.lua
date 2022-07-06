73cadf7e31522806f478af4e09ab590460816cfb
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-07-07 01:33:26 +0300

    [Sync] Thu 07 Jul 2022 01:32:47 AM MSK

--- configs/nvim/lua/user/cmake.lua
+++ configs/nvim/lua/user/cmake.lua
@@ -24,7 +24,8 @@ g.cmake_default_config = 'Debug'
 -- .git determines the root of the project
 -- this is searched relative to the cwd
 g.cmake_root_markers = {
-    '.git'
+    '.git',
+    'src/'
 }
 
 -- All build configs will be created inside build dir of the project
