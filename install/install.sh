443b303d6701f5b54b18ec56bc22221be0b8083a
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-10-16 15:22:53 +0400

    wip: add notes on creating installation script

---
+++ install/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1. fix .zprofile
+# 2. create dirs (for ex. for xmonad)
+# mkdir -p "$XDG_CACHE_HOME"/xmonad
+# mkdir -p "$XDG_CONFIG_HOME"/xmonad
+# mkdir -p "$XDG_DATA_HOME"/xmonad
+
+# install CPP header if have sudo rights
+
+# PG
+# mkdir "$XDG_CONFIG_HOME/pg" && mkdir "$XDG_STATE_HOME"
+
+# install all needed programs
+# macos -> with brew
+# arch -> pacman
+
+#### UNINSTALL
+# unlink all dirs
