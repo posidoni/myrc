3fd7fb1d069036477916887a1122bc316b847206
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-11-29 14:25:45 +0400

    ref(vim/go): change go helper plugin

--- configs/nvim/lua/settings/lsp/configs/gopls.lua
+++ configs/nvim/lua/settings/lsp/configs/gopls.lua
@@ -1,6 +1,7 @@
 return {
     settings = {
         gopls = {
+            gofumpt = true,
             hints = {
                 compositeLiteralFields = true,
                 assignVariableTypes = true,
