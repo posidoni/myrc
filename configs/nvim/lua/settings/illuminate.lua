f0f72fbe91ef7597bb750224761156ee80fe5af3
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-12-18 14:53:18 +0400

    vim: add illuminate plugin

---
+++ configs/nvim/lua/settings/illuminate.lua
@@ -0,0 +1,39 @@
+local ok, illuminate = pcall(require, 'illuminate')
+if not ok then
+    vim.notify('Failed to load illuminate', 'error')
+end
+
+-- default configuration
+illuminate.configure({
+    providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+    },
+    delay = 120,
+    filetype_overrides = {},
+    filetypes_denylist = {
+        'dirvish',
+        'fugitive',
+        'alpha',
+        'NvimTree',
+        'packer',
+        'neogitstatus',
+        'Trouble',
+        'lir',
+        'Outline',
+        'spectre_panel',
+        'toggleterm',
+        'DressingSelect',
+        'TelescopePrompt',
+    },
+    filetypes_allowlist = {},
+    modes_denylist = {},
+    modes_allowlist = {},
+    providers_regex_syntax_denylist = {},
+    providers_regex_syntax_allowlist = {},
+    under_cursor = true,
+    large_file_cutoff = nil,
+    large_file_overrides = nil,
+    min_count_to_highlight = 1,
+})
