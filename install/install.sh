95667f094ee63a6b43c7bfb4a086cd2010c17975
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-12-18 14:19:14 +0400

    feat: add vim plugins sync to install script

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Install plugin managers
-[ ! -d "$HOME/.local/share/tmux/tpm" ] && git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/tpm
-[ ! -d "$HOME/.local/share/nvim/site/pack/packer" ] && git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+[ ! -d "$XDG_DATA_HOME/tmux/tpm" ] && git clone https://github.com/tmux-plugins/tpm "$XDG_DATA_HOME/tmux/tpm"
+[ ! -d "$XDG_DATA_HOME/nvim/site/pack/packer" ] && git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ "$XDG_DATA_HOME/nvim/site/pack/packer/start/packer.nvim"
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
