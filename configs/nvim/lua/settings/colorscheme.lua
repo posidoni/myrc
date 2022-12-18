9ce8fa5f2ceec385c2c87789324793e7f7bc46e0
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-12-18 14:53:32 +0400

    vim: refactor colorschemes config

--- configs/nvim/lua/settings/colorscheme.lua
+++ configs/nvim/lua/settings/colorscheme.lua
@@ -1,62 +1,57 @@
-require("onedark").setup({
-    -- Main options --
-    style = "deep", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = false, -- Show/hide background
-    term_colors = true, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+local M = {}
 
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
-    code_style = {
-        comments = "none",
-        keywords = "bold",
-        functions = "bold",
-        strings = "none",
-        variables = "none",
-    },
-    -- Plugins Config --
-    diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = true, -- use background color for virtual text
-    },
-})
+M.tokyo = function()
+    local ok_t, tokyo = pcall(require, 'tokyonight')
+    if not ok_t then
+        vim.notify('Failed to init tokyonight')
+    end
+    tokyo.setup({
+        style = 'night',
+        transparent = false,
+        terminal_colors = true,
+        styles = {
+            comments = { italic = false },
+            keywords = { italic = false },
+            functions = {},
+            variables = {},
+            sidebars = 'dark',
+            floats = 'dark',
+        },
+        sidebars = { 'qf', 'help' },
+        day_brightness = 0.3,
+        hide_inactive_statusline = false,
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+    })
+end
 
--- Example config in Lua
-vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
---
-require("onedark").load()
---
--- require("doom-one").setup({
--- 	cursor_coloring = true,
--- 	terminal_colors = true,
--- 	italic_comments = true,
--- 	enable_treesitter = true,
--- 	transparent_background = false, -- doesn't work & break things
--- 	pumblend = {
--- 		enable = true,
--- 		transparency_amount = 20,
--- 	},
--- 	plugins_integrations = {
--- 		neorg = true,
--- 		barbar = true,
--- 		bufferline = true,
--- 		gitgutter = false,
--- 		gitsigns = true,
--- 		telescope = true,
--- 		neogit = true,
--- 		nvim_tree = true,
--- 		dashboard = true,
--- 		startify = true,
--- 		whichkey = true,
--- 		indent_blankline = true,
--- 		vim_illuminate = true,
--- 		lspsaga = false,
--- 	},
--- })
+M.onedark = function()
+    local ok, onedark = pcall(require, 'onedark')
+    if not ok then
+        vim.notify('Failed to init colorscheme')
+    end
+    onedark.setup({
+        style = 'deep',
+        transparent = false,
+        term_colors = true,
+        ending_tildes = false,
+        cmp_itemkind_reverse = false,
+        code_style = {
+            comments = 'none',
+            keywords = 'bold',
+            functions = 'bold',
+            strings = 'none',
+            variables = 'none',
+        },
+        diagnostics = {
+            darker = true,
+            undercurl = true,
+            background = true,
+        },
+    })
+end
+
+M.onedark()
+M.tokyo()
+
+return M
