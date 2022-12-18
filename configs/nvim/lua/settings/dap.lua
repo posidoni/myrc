1a6d45f1627a831110210d6d4b0b40e2ea17b8fa
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-12-18 14:53:23 +0400

    vim: reconfigure dap

--- configs/nvim/lua/settings/dap.lua
+++ configs/nvim/lua/settings/dap.lua
@@ -1,74 +1,86 @@
-local dap_ok, dap = pcall(require, "dap")
+local dap_ok, dap = pcall(require, 'dap')
 if not dap_ok then
-    vim.notify("Dap not found please install it!")
+    vim.notify('Dap not found', 'error')
     return
 end
+local dapui_ok, dapui = pcall(require, 'dapui')
+if not dapui_ok then
+    vim.notify('DapUI not found', 'error')
+    return
+end
+local dap_vtext_ok, dap_vtext = pcall(require, 'nvim-dap-virtual-text')
+if not dap_vtext_ok then
+    vim.notify('Dap Virtual Text not found please install it!')
+    return
+end
+local ok, tscope = pcall(require, 'telescope')
+if ok then
+    tscope.load_extension('dap')
+end
 
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true }
+local set = vim.keymap.set
+local opts = { noremap = false, silent = true }
 
-map("n", "<leader>bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
-map("n", "<leader>bl", "<cmd>lua require'dap'.list_breakpoints()<cr>", opts)
-map("n", "<leader>bc", "<cmd>lua require'dap'.clear_breakpoints()<cr>", opts)
-map("n", "<leader>B", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
-map("n", "<leader>si", "<cmd>lua require'dap'.step_into()<cr>", opts)
-map("n", "<leader>ss", "<cmd>lua require'dap'.step_over()<cr>", opts)
-map("n", "<leader>so", "<cmd>lua require'dap'.step_out()<cr>", opts)
-map("n", "<leader>sb", "<cmd>lua require'dap'.step_back()<cr>", opts)
-map("n", "<leader>co", "<cmd>lua require'dap'.continue()<cr>", opts)
-map("n", "<leader>cl", "<cmd>lua require'dap'.terminate()<cr>", opts)
-map("n", "<leader>\\", "<cmd>lua require'dapui'.toggle()<cr>", opts)
+set('n', '<leader>bb', dap.toggle_breakpoint, opts)
+set('n', '<leader>bl', dap.list_breakpoints, opts)
+set('n', '<leader>bc', dap.clear_breakpoints, opts)
+set('n', '<leader>B', function()
+    dap.set_breakpoint(vim.fn.input('Breakpoint condition:'))
+end, opts)
+set('n', '<leader>si', dap.step_into, opts)
+set('n', '<leader>ss', dap.step_over, opts)
+set('n', '<leader>so', dap.step_out, opts)
+set('n', '<leader>sb', dap.step_back, opts)
+set('n', '<leader>co', dap.continue, opts)
+set('n', '<leader>cl', dap.terminate, opts)
+set('n', '<leader>\\', dapui.toggle, opts)
 
 require('dap-go').setup({
-    on_attach = function()
-    end
+    on_attach = function() end,
 })
 
-dap.adapters.lldb = { type = "executable", command = 'lldb-vscode', name = "lldb" }
--- Dap Setup
-
-dap.configurations.cpp = {
-    {
-        name = "Launch",
-        type = "lldb",
-        request = "launch",
-        program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
+local conf = {
+    adapters = {
+        lldb = { type = 'executable', command = 'lldb-vscode', name = 'lldb' },
+    },
+    configurations = {
+        cpp = {
+            {
+                name = 'Launch',
+                type = 'lldb',
+                request = 'launch',
+                program = function()
+                    return vim.fn.input(
+                        'Path to executable: ',
+                        vim.fn.getcwd() .. '/',
+                        'file'
+                    )
+                end,
+                cwd = '${workspaceFolder}',
+                stopOnEntry = false,
+                args = {},
+            },
+        },
     },
 }
+conf.configurations.c = conf.configurations.cpp
 
-dap.configurations.c = dap.configurations.cpp
-
--- 
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
-
-local dapui_ok, dapui = pcall(require, "dapui")
-if not dapui_ok then
-    vim.notify("Dapui not found please install it!")
-    return
-end
+vim.fn.sign_define(
+    'DapBreakpoint',
+    { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' }
+)
 
 dapui.setup()
-
-local dap_vtext_ok, dap_vtext = pcall(require, "nvim-dap-virtual-text")
-if not dap_vtext_ok then
-    vim.notify("Dap Virtual Text not found please install it!")
-    return
-end
-
 dap_vtext.setup()
 
--- Open UI on debugging start
-dap.listeners.after.event_initialized["dapui_config"] = function()
+dap.listeners.after.event_initialized['dapui_config'] = function()
     dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.event_terminated['dapui_config'] = function()
     dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited['dapui_config'] = function()
     dapui.close()
 end
+
+vim.tbl_deep_extend('force', dap, conf)
