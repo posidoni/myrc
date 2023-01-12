4083262c1a8ed41e1c42332da73d51a2a3ba1070
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2023-01-12 20:05:10 +0400

    zsh: fix completion

--- configs/zsh/comp_setup.sh
+++ configs/zsh/comp_setup.sh
@@ -1,45 +1,54 @@
 #!/bin/zsh
 
-######
-zstyle ':completion:*' special-dirs true
-
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+# Awesome configuration guide:
+# https://thevaluable.dev/zsh-completion-guide-examples/
 
 zmodload -i zsh/complist
+autoload -U compaudit compinit && compinit -u -d "$ZSH_COMPDUMP"
+WORDCHARS=''
+autoload -U +X bashcompinit && bashcompinit
 
+setopt auto_menu
+setopt complete_in_word
+setopt always_to_end
 
-autoload -U compinit; compinit
-
-zstyle ':completion:*:*:cp:*' file-sort size
-
-zstyle ':completion:*' completer _extensions _complete _approximate
-
-zstyle ':completion:*' use-cache on
-
-zstyle ':completion:*' menu select
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d -!%f'
-
-zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-
+zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' group-name ''
 
-zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
-zmodload zsh/complist
+zstyle ':completion:*' use-cache yes
+zstyle ':completion:*' cache-path $ZSH_COMPDUMP
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:cp:*' file-sort size
+zstyle ':completion:*' completer _extensions _complete _approximate
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' menu select
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d -!%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*' file-sort modification
+
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 
-zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-zstyle ':completion:*' file-sort modification
-######
-
-bindkey "^F" fzf-file-widget
+bindkey "^R" history-incremental-search-backward
 bindkey "^H" backward-kill-word
 bindkey "^N" down-line-or-history
 bindkey "^P" up-line-or-history
+
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
+
