2d70497c184109bbe8c2f933d392706258d76ae2
Mikhail K <mikhail.n.kuznetsov@gmail.com>
2023-02-05 21:29:57 +0400

    osx: add script to disable `press & hold chars`

---
+++ local/bin/fix_press_and_hold_osx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# https://apple.stackexchange.com/questions/332769/macos-disable-popup-showing-accented-characters-when-holding-down-a-key
+defaults write -g ApplePressAndHoldEnabled -bool false
